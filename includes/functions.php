1fb381e56f11c40810e8415d2bd7fc3f998e2435
Matthias Pfefferle <pfefferle@gmail.com>
2018-09-07 09:11:21 +0200

    oops

--- includes/functions.php
+++ includes/functions.php
@@ -39,7 +39,7 @@ if ( ! function_exists( 'url_to_authorid' ) ) :
 
 		// match the rewrite rule with the passed url
 		if ( preg_match( '/https?:\/\/(.+)' . preg_quote( $author_regexp, '/' ) . '([^\/]+)/i', $url, $match ) ) {
-			$user = get_user_by( 'slug', $match[2] )
+			$user = get_user_by( 'slug', $match[2] );
 			if ( $user ) {
 				return $user->ID;
 			}
